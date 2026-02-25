1a05122fa1468ac725091e39c483c17aae7bcb98
nathanjnorris <13533617+nathanjnorris@users.noreply.github.com>
2026-02-25 10:36:16 +1000

    test: trigger caller workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@
 # Add your Azure resources here
 #
 # Example:
-# resource "azurerm_resource_group" "main" {
-#   name     = var.resource_group_name
-#   location = var.location
-#   tags     = var.tags
-# }
+resource "azurerm_resource_group" "main" {
+  name     = "test-code"
+  location = var.location
+  tags     = var.tags
+}
 # ==============================================================================
 
 # Placeholder - Add your resources here
