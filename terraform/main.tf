bf28aff09ccdcbead4ddcaa5074235acf80b714f
Nathan <13533617+nathanjnorris@users.noreply.github.com>
2026-02-25 14:46:43 +1000

    fix: restore template

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,10 @@
 # Add your Azure resources here
 #
 # Example:
-resource "azurerm_resource_group" "main" {
-  name     = "test-code"
-  location = var.location
-  tags     = var.tags
+# resource "azurerm_resource_group" "main" {
+#   name     = var.resource_group_name
+#   location = var.location
+#   tags     = var.tags
 }
 # ==============================================================================
 
